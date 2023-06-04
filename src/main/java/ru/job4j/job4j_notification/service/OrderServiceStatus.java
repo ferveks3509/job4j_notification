23e8021da72beef832330ae6cdd78215ac87db0f
ferveks3509 <qq.ferveks@gmail.com>
2023-06-04 17:54:26 +0300

    add Notification model and fix kafka messages

--- src/main/java/ru/job4j/job4j_notification/service/OrderServiceStatus.java
+++ src/main/java/ru/job4j/job4j_notification/service/OrderServiceStatus.java
@@ -2,12 +2,13 @@ package ru.job4j.job4j_notification.service;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import ru.job4j.job4j_notification.model.Notification;
 
 @Service
 public class OrderServiceStatus {
 
     @KafkaListener(topics = "dishStatus")
-    public void receiveOrder(String status) {
-        System.out.println(status);
+    public void receiveOrder(Notification notification) {
+        System.out.println(notification.getStatus());
     }
 }
